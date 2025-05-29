30f4fea3ce81127da4b4b165d2c35e0472b4dce5
Playerb96 <player.b.96@hotmail.com>
2025-05-29 12:10:16 -0500

    validation para prod

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import { NextConfig as _NextConfig } from "next"; // Import para usar en runtime
 
 const nextConfig: _NextConfig = {
   images: {
-    domains: ["pub-b150312a074447b28b7b2fe8fac4e6f5.r2.dev"],
+    domains: [
+      "pub-b150312a074447b28b7b2fe8fac4e6f5.r2.dev",
+      "localhost",
+      "127.0.0.1",
+    ],
   },
   async redirects() {
     return [
