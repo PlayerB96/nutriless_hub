75c1388e5552876713c97cecb34bb8b3223fe982
Playerb96 <player.b.96@hotmail.com>
2025-05-29 12:27:55 -0500

    v conig

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import { NextConfig as _NextConfig } from "next"; // Import para usar en runtime
 
 const nextConfig: _NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.vercel.app",
+        hostname: "https://pub-b150312a074447b28b7b2fe8fac4e6f5.r2.dev",
+        port: "3000",
+        pathname: "/api/image/**",
       },
     ],
-    domains: ["pub-b150312a074447b28b7b2fe8fac4e6f5.r2.dev"],
   },
 
   async redirects() {
