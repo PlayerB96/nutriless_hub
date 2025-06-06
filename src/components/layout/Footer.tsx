c38d76ee757aad22a7138097f1b5f514156cef48
playerb96 <player.b.96@hotmail.com>
2025-06-06 16:46:12 -0500

    validacion de aliknetos organicos y recetas creadion v0.1.2

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
     <footer className="bg-muted dark:bg-muted p-4 text-center text-sm text-text-secondary dark:text-text-secondary">
-      © {new Date().getFullYear()} NutriHub v0.1.1 Todos los derechos reservados.
+      © {new Date().getFullYear()} NutriHub v0.1.2 Todos los derechos
+      reservados.
     </footer>
   );
 }
